8d1dcea1bf8cf6f32921f319cfd324795bcbb658
Shahab Kondri <shahab.kondri@gmail.com>
2026-06-02 09:30:50 -0400

    Add App Store review flow

--- Prometheus Formatter/Resources/Script.js
+++ Prometheus Formatter/Resources/Script.js
@@ -1,9 +1,9 @@
 function show(enabled, useSettingsInsteadOfPreferences) {
 	if (useSettingsInsteadOfPreferences) {
-		document.querySelector('.state-on').innerText = "🚀 Prometheus Formatter is active! Metrics are highlighted and ready for action. Explore your enhanced metrics view below.";
-		document.querySelector('.state-off').innerText = "🛠️ Prometheus Formatter is currently inactive. Activate it in Safari Settings > Extensions to enhance your metrics view!";
-		document.querySelector('.state-unknown').innerText = "🔍 Ready to spotlight those Prometheus metrics? Enable Prometheus Formatter in Safari Settings > Extensions!";
-		document.querySelector('.open-preferences').innerText = "⚙️ Open Safari Extensions Preferences";
+		document.querySelector('.state-on').innerText = "Prometheus Formatter is active.";
+		document.querySelector('.state-off').innerText = "Prometheus Formatter is inactive. Enable it in Safari Settings > Extensions.";
+		document.querySelector('.state-unknown').innerText = "Enable Prometheus Formatter in Safari Settings > Extensions.";
+		document.querySelector('.open-preferences .button-label').innerText = "Open Safari Extensions Preferences";
 	}
 
 	if (typeof enabled === "boolean") {
@@ -19,4 +19,9 @@ function openPreferences() {
 	webkit.messageHandlers.controller.postMessage("open-preferences");
 }
 
+function openReviewPage() {
+	webkit.messageHandlers.controller.postMessage("open-review-page");
+}
+
 document.querySelector("button.open-preferences").addEventListener("click", openPreferences);
+document.querySelector("button.open-review-page").addEventListener("click", openReviewPage);
