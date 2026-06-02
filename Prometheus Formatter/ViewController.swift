91c99b3f1ccd40a0440bcf21170cfbc49adaf173
Shahab Kondri <shahab.kondri@gmail.com>
2026-06-02 10:04:10 -0400

    Add App Store review flow (#8)

--- Prometheus Formatter/ViewController.swift
+++ Prometheus Formatter/ViewController.swift
@@ -1,12 +1,19 @@
 import Cocoa
 import SafariServices
+import StoreKit
 import WebKit
 
 let extensionBundleIdentifier = "com.shahabkondri.prometheus-formatter.extension"
+let appStoreReviewURL = URL(string: "macappstore://apps.apple.com/app/id6738227397?action=write-review")!
+let appStoreReviewFallbackURL = URL(string: "https://apps.apple.com/app/id6738227397?action=write-review")!
 
 class ViewController: NSViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
     @IBOutlet var webView: WKWebView!
+    private let activeLaunchCountKey = "review.activeLaunchCount"
+    private let lastVersionPromptedForReviewKey = "review.lastVersionPromptedForReview"
+    private let minimumActiveLaunchesBeforeReviewPrompt = 3
+    private var didScheduleReviewRequest = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,19 +38,69 @@ class ViewController: NSViewController, WKNavigationDelegate, WKScriptMessageHan
                 } else {
                     webView.evaluateJavaScript("show(\(state.isEnabled), false)")
                 }
+
+                self.requestReviewAfterSustainedUseIfAppropriate(isExtensionEnabled: state.isEnabled)
             }
         }
     }
 
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if (message.body as! String != "open-preferences") {
-            return;
+        guard let action = message.body as? String else {
+            return
         }
 
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
-            DispatchQueue.main.async {
-                NSApplication.shared.terminate(nil)
+        switch action {
+        case "open-preferences":
+            SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+                DispatchQueue.main.async {
+                    NSApplication.shared.terminate(nil)
+                }
             }
+        case "open-review-page":
+            if !NSWorkspace.shared.open(appStoreReviewURL) {
+                NSWorkspace.shared.open(appStoreReviewFallbackURL)
+            }
+        default:
+            return
+        }
+    }
+
+    private func requestReviewAfterSustainedUseIfAppropriate(isExtensionEnabled: Bool) {
+        guard isExtensionEnabled, !didScheduleReviewRequest else {
+            return
+        }
+
+        let defaults = UserDefaults.standard
+        let activeLaunchCount = defaults.integer(forKey: activeLaunchCountKey) + 1
+        defaults.set(activeLaunchCount, forKey: activeLaunchCountKey)
+
+        guard activeLaunchCount >= minimumActiveLaunchesBeforeReviewPrompt else {
+            return
+        }
+
+        let currentVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "unknown"
+        guard defaults.string(forKey: lastVersionPromptedForReviewKey) != currentVersion else {
+            return
+        }
+
+        didScheduleReviewRequest = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+            guard let self = self, self.view.window?.isVisible == true else {
+                return
+            }
+
+            self.requestAppStoreReview()
+            defaults.set(currentVersion, forKey: self.lastVersionPromptedForReviewKey)
+        }
+    }
+
+    private func requestAppStoreReview() {
+        if #available(macOS 15.0, *) {
+            Task { @MainActor in
+                AppStore.requestReview(in: self)
+            }
+        } else if #available(macOS 10.14, *) {
+            SKStoreReviewController.requestReview()
         }
     }
 
